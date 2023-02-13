61d5c64563717d489d8a8c25663360387077ce93
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-13 21:44:50 +0000

    This is my java projects

---
+++ FirstClass.java
@@ -0,0 +1,25 @@
+package firstp;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+        System.out.println("How was your day?");
+    }
+}
+class SecondClass{
+
+    public static void main(String[] args) {
+        System.out.println("olla");
+        System.out.println("this is java learning");
+    }
+}
+
+class ThirdClass{
+    public static void main(String[] args) {
+        System.out.println("hhjhjhhhafh");
+        System.out.println("jajgjghjsjdghas");
+
+    }
+}
---
+++ New.java
@@ -0,0 +1,14 @@
+package firstp;
+
+public class New {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("how are you doing");
+        System.out.println("How was your interview yesterday");
+
+
+    }
+
+
+}
