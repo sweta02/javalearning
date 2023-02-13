069d8c4bf85a2a68663e4b3ebcb237b97000a253
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-13 22:42:24 +0000

    newrepo

--- NewRepo.java
+++ NewRepo.java
@@ -5,5 +5,11 @@ public class NewRepo {
     public static void main(String[] args) {
         System.out.println("This is first repository");
 
+        /* i am trying to add to git hub
+        and i have to use git commands to push the changes in git hub
+         */
+
+        System.out.println("new push to git hub");
+
     }
 }
